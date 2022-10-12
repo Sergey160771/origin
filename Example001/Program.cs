371919e90cf53f088415abbaf19deddac673e8a5
Sergy <9058591133@mail.ru>
2022-10-12 09:18:23 +0500

    Исправление программы

--- Example001/Program.cs
+++ Example001/Program.cs
@@ -25,11 +25,12 @@ void CreateArrayLessThanEqualThreeCharacters(string[] array, string[] array1)
 }
 void PrintArray(string[] array)
 {
+Console.Write("[");   
     for (int i = 0; i < array1.Length; i++)
     {
-        Console.Write($" {array1[i]} ");
+        Console.Write($" {array1[i]}");
     }
-
+Console.Write("]");
 }
 CreateArrayLessThanEqualThreeCharacters(array, array1);
 Console.WriteLine();
